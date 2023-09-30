3471fccca614eb4ec7563641d753ee3f920ce03b
devCarlosSantoss <carlosvinicius357890@gmail.com>
2023-09-30 11:49:34 -0300

    Adição de novos exercicios

---
+++ orientacao_a_objetos/src/Carro.java
@@ -0,0 +1,13 @@
+public class Carro extends Veiculo{
+    public void ligar(){
+        confereCambio();
+        confereCombustivel();
+        System.out.println("Carro ligado!");
+    }
+    private void confereCombustivel(){
+        System.out.println("Conferindo Combustível");
+    }
+    private void confereCambio(){
+        System.out.println("Conferindo cambio em P");
+    }
+}
---
+++ orientacao_a_objetos/src/Veiculo.java
@@ -0,0 +1,11 @@
+public class Veiculo {
+    private String chassi;
+
+    public String getChassi(){
+        return this.chassi;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+}
